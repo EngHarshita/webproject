9e4faa119d038e895a90546bfe055c8a7f6f52ca
EngHarshita <harshitapal5678@gmail.com>
2026-05-02 00:14:19 -0700

    Fix Express 5 wildcard route for deployment

--- server/app.js
+++ server/app.js
@@ -450,7 +450,7 @@ io.on('connection', socket => {
 // --- PRODUCTION SETUP FOR UNIFIED DEPLOYMENT (RAILWAY/RENDER) ---
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
-    app.get('*', (req, res) => {
+    app.get('(.*)', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
     });
 }
